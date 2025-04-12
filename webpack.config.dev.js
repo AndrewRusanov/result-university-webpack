48eb2f0e701bff31ae7e5c849c442378c8b01a61
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-04-12 15:08:55 +0300

    feat: добавляет eslint в dev-режим сборки

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,5 +1,7 @@
 const { default: merge } = require("webpack-merge");
+const ESLintPlugin = require("eslint-webpack-plugin");
 const commonConfig = require("./webpack.config");
+const path = require("path");
 
 module.exports = merge(commonConfig, {
   mode: "development",
@@ -8,4 +10,15 @@ module.exports = merge(commonConfig, {
     hot: true,
     open: true,
   },
+  plugins: [
+    new ESLintPlugin({
+      context: path.resolve(__dirname, "src"),
+      extensions: ["js"],
+      exclude: ["node_modules"],
+      fix: true,
+      cache: true,
+      emitWarning: true,
+      failOnError: false,
+    }),
+  ],
 });
