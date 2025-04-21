6d0fc986ebafbcd29aba5dfed1872acd94374574
Andrew Rusanov <rusanow.andrej2018@yandex.ru>
2025-04-21 19:39:16 +0300

    feat: migrate index.js to index.ts

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 module.exports = {
   context: path.resolve(__dirname, "src"),
-  entry: "./index.js",
+  entry: "./index.ts",
   output: {
     filename: "[name].[contenthash:8].js",
     path: path.resolve(__dirname, "dist"),
